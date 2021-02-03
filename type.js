9c0bc05fa814bd38deb8bd9b6b1f2a599b51947c
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-02-03 17:33:38 -0500

    Update type.js

--- type.js
+++ type.js
@@ -1,12 +1,6 @@
 var app = document.getElementById('subtitle');
-var app2 = document.getElementById('box');
 
 var typewriter = new Typewriter(app, {
-  loop: false,
-  delay: 75,
-});
-
-var typewriter2 = new Typewriter(app, {
   loop: false,
   delay: 75,
 });
@@ -23,11 +17,5 @@ typewriter
   .pauseFor(350)
   .deleteChars(3)
   .typeString('oder')
-  .pauseFor(1500)
-  .start();
-
-typewriter2
-  .pauseFor(150)
-  .typeString('I\'m Fisher, A simple human that scripts and codes in roblox and node.js. You can check out some of my profiles below!')
   .pauseFor(1500)
   .start();
