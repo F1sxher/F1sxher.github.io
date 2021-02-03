0ce6b096f9e982c266813d977e38e2e262f03bec
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-02-03 17:26:20 -0500

    Create typemain.js

---
+++ typemain.js
@@ -0,0 +1,21 @@
+var app = document.getElementById('box');
+
+var typewriter = new Typewriter(app, {
+  loop: false,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(150)
+  .typeString('Roblox Builder')
+  .pauseFor(1500)
+  .deleteChars(14)
+  .typeString('Roblox Scripter')
+  .pauseFor(1500)
+  .deleteChars(15)
+  .typeString('Node.js Cder')
+  .pauseFor(350)
+  .deleteChars(3)
+  .typeString('oder')
+  .pauseFor(1500)
+  .start();
